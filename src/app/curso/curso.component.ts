78734e7872ae01ada7fb78426011a7e708db0afb
IasArchi <iasmimcardoso11@gmail.com>
2022-10-02 14:33:38 -0300

    PROJETO API ANGULAR

---
+++ src/app/curso/curso.component.ts
@@ -0,0 +1,21 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-curso',
+  templateUrl: './curso.component.html',
+  styleUrls: ['./curso.component.css']
+})
+export class CursoComponent implements OnInit {
+
+  nome:string = "Ralf";
+
+  //Construtor  
+    
+  constructor() { }
+    
+  //Inicializador
+
+  ngOnInit(): void {
+  }
+
+}
